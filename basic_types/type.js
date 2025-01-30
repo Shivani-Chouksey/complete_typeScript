3b99870127419c90c26643d02193d78d65fde897
Shivani-Chouksey <shivanichouksey1702@gmail.com>
2025-01-30 12:47:20 +0530

    how to assign types in variable

---
+++ basic_types/type.js
@@ -0,0 +1,10 @@
+// #########   The primitives:string,numberand boolean   #########
+//number
+var userId = 324342;
+// If write tha userId and dot notation is show all the methods and values related to number in javascript only show those methods
+// userId = "shivani";
+// it gives scugly Error that
+//  Type 'string' is not assignable to type 'number'.
+// it may be number or float any  - - everything is simply number  inside javascript
+//Boolean
+var IsLoggedIn = false;
---
+++ basic_types/type.ts
@@ -0,0 +1,16 @@
+// #########   The primitives:string,numberand boolean   #########
+
+//number
+let userId: number = 324342;
+
+// If write tha userId and dot notation is show all the methods and values related to number in javascript only show those methods
+
+// userId = "shivani";
+// it gives scugly Error that
+//  Type 'string' is not assignable to type 'number'.
+// you should'nt be do it /
+
+// it may be number or float any  - - everything is simply number  inside javascript
+
+//Boolean
+let IsLoggedIn: boolean = false;

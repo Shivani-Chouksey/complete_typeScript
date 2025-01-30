bb68c4b8c90e698291a5fb50e4e3e8a8bcb49b01
Shivani-Chouksey <shivanichouksey1702@gmail.com>
2025-01-30 16:55:53 +0530

    array

---
+++ basic_types/array/array.js
@@ -0,0 +1,9 @@
+var superHeros = [];
+// const herPower: number[] = [];
+var heroPower = []; // define also using that
+//array inside array type declartion
+var MLModels = [[2, 3, 3]];
+var allUsers = [];
+superHeros.push("Fdsfds");
+heroPower.push(767);
+allUsers.push({ name: "", isActive: true });
---
+++ basic_types/array/array.ts
@@ -0,0 +1,17 @@
+const superHeros: string[] = [];
+// const herPower: number[] = [];
+const heroPower: Array<number> = []; // define also using that
+
+type UserDetail = {
+  name: string;
+  isActive: boolean;
+};
+
+//array inside array type declartion
+const MLModels: number[][] = [[2, 3, 3]];
+
+const allUsers: UserDetail[] = [];
+
+superHeros.push("Fdsfds");
+heroPower.push(767);
+allUsers.push({ name: "", isActive: true });

066c14f4049b9cea91694f667cc59ab0c839a1d8
Shivani-Chouksey <shivanichouksey1702@gmail.com>
2025-01-30 14:32:45 +0530

    objects

---
+++ basic_types/objects/object.js
@@ -0,0 +1,10 @@
+var User = {
+    name: "shivani",
+    email: "email.com",
+    isActive: true
+};
+// function createUser({ name: string, isPaid: boolean }) {}
+function createCourse() {
+    return { name: "chouksey", price: 463245 };
+}
+createUser({ name: "chouksey", isPaid: 463245 });
---
+++ basic_types/objects/object.ts
@@ -0,0 +1,12 @@
+const User = {
+  name: "shivani",
+  email: "email.com",
+  isActive: true,
+};
+
+// function createUser({ name: string, isPaid: boolean }) {}
+
+function createCourse(): { name: string; price: number } {
+  return { name: "chouksey", price: 463245 };
+}
+// createUser({ name: "chouksey", isPaid: 463245 });

f736f4a7ad466f608783d8d5b716168246db24d7
Shivani-Chouksey <shivanichouksey1702@gmail.com>
2025-01-31 16:19:15 +0530

    classes

--- setUp_For_Project/dist/index.js
+++ setUp_For_Project/dist/index.js
@@ -1,2 +1,12 @@
 "use strict";
-console.log("TypeScript is here");
+class User {
+    constructor(email, name) {
+        this.city = "";
+        this.dob = "22/22/22";
+        this.email = email;
+        this.name = name;
+    }
+}
+const shivani = new User("@gmail.com", "ghfaghf");
+shivani.city = "mumbai";
+// shivani.dob = "fdsfd";   // show error
--- setUp_For_Project/src/index.ts
+++ setUp_For_Project/src/index.ts
@@ -1,1 +1,14 @@
-console.log("TypeScript is here");
+class User {
+  email: string;
+  name: string;
+  city: string = "";
+  readonly dob: string = "22/22/22";
+  constructor(email: string, name: string) {
+    this.email = email;
+    this.name = name;
+  }
+}
+
+const shivani = new User("@gmail.com", "ghfaghf");
+shivani.city = "mumbai";
+// shivani.dob = "fdsfd";   // show error

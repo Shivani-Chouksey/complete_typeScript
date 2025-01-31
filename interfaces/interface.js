0da803ebd176e6c1d0228d03bc9266e9cbae2fd2
Shivani-Chouksey <shivanichouksey1702@gmail.com>
2025-01-31 13:56:47 +0530

    how to add additinal value on ecisting interface

--- interfaces/interface.js
+++ interfaces/interface.js
@@ -1,7 +1,21 @@
 // Give a overview the deatils what needed
 var hitesh = {
     dbId: 34654,
-    email: ".com",
+    email: "gmail.com",
+    githubId: "githubId",
+    password: 66565,
+    startTrail: function () {
+        return "trail started";
+    },
+    getCoupan: function (name, value) {
+        return 10;
+    }
+};
+var chouksey = {
+    dbId: 34654,
+    role: "admin",
+    email: "gmail.com",
+    githubId: "githubId",
     password: 66565,
     startTrail: function () {
         return "trail started";
--- interfaces/interface.ts
+++ interfaces/interface.ts
@@ -10,9 +10,34 @@ interface loginDetail {
   getCoupan(name: string, value: number): number;
 }
 
+// if add additional detail , it is also a good to add additonal value inside existing interface
+interface loginDetail {
+  githubId: string;
+}
+
+//inheritance
+interface AdminDetail extends loginDetail {
+  role: "admin" | "ta";
+}
+
 const hitesh: loginDetail = {
   dbId: 34654,
-  email: ".com",
+  email: "gmail.com",
+  githubId: "githubId",
+  password: 66565,
+  startTrail: () => {
+    return "trail started";
+  },
+  getCoupan: (name: "chouksey", value: 12) => {
+    return 10;
+  },
+};
+
+const chouksey: AdminDetail = {
+  dbId: 34654,
+  role: "admin",
+  email: "gmail.com",
+  githubId: "githubId",
   password: 66565,
   startTrail: () => {
     return "trail started";

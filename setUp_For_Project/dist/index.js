7a5a0852783dee8dbea5c4df44157b59a98a0250
Shivani-Chouksey <shivanichouksey1702@gmail.com>
2025-01-31 16:27:47 +0530

    public and private

--- setUp_For_Project/dist/index.js
+++ setUp_For_Project/dist/index.js
@@ -2,11 +2,21 @@
 class User {
     constructor(email, name) {
         this.city = "";
-        this.dob = "22/22/22";
+        this.dob = "22/22/22"; // using private variable dob not accessable outof the class only access that variable inside the class only
         this.email = email;
         this.name = name;
+        this.dob; ///  that is correct able to assign dob value
     }
 }
 const shivani = new User("@gmail.com", "ghfaghf");
 shivani.city = "mumbai";
-// shivani.dob = "fdsfd";   // show error
+// shivani.dob = "fdsfd";   // show error - you can not assign value
+// shivani.dob // --- show error and , not able to access dob outside the class
+class UserDetail {
+    constructor(email, name, userId) {
+        this.email = email;
+        this.name = name;
+        this.userId = userId;
+        this.city = "";
+    }
+}
--- setUp_For_Project/src/index.ts
+++ setUp_For_Project/src/index.ts
@@ -1,14 +1,27 @@
 class User {
-  email: string;
+  public email: string;
   name: string;
   city: string = "";
-  readonly dob: string = "22/22/22";
+  private readonly dob: string = "22/22/22"; // using private variable dob not accessable outof the class only access that variable inside the class only
   constructor(email: string, name: string) {
     this.email = email;
     this.name = name;
+    this.dob; ///  that is correct able to assign dob value
   }
 }
 
 const shivani = new User("@gmail.com", "ghfaghf");
 shivani.city = "mumbai";
-// shivani.dob = "fdsfd";   // show error
+// shivani.dob = "fdsfd";   // show error - you can not assign value
+
+// shivani.dob // --- show error and , not able to access dob outside the class
+
+class UserDetail {
+  readonly city: string = "";
+
+  constructor(
+    public email: string,
+    public name: string,
+    private userId: number
+  ) {}
+}
